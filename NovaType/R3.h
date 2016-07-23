56a372ba045042d7e7048d03bcfac71ecd2c013b
Chong Luo <noteasy2beme@gmail.com>
2016-07-23 00:36:53 -0700

    Finished R3(3D vector) class

--- NovaType/R3.h
+++ NovaType/R3.h
@@ -1,6 +1,8 @@
 #ifndef __R3__
 #define __R3__
 
+#include <cmath>
+
 namespace NovaType
 {
     class R3
@@ -47,67 +49,114 @@ namespace NovaType
 
         const R3 &operator =(const R3 &data)
         {
-            this->m_coord[0] = data.m_coord[0];
+            m_coord[0] = data.m_coord[0];
 
-            this->m_coord[1] = data.m_coord[1];
+            m_coord[1] = data.m_coord[1];
 
-            this->m_coord[2] = data.m_coord[2];
-
-            return *this;
-        }
-
-        const R3 &operator +=(const R3 &data)
-        {
-            this->m_coord[0] += data.m_coord[0];
-
-            this->m_coord[1] += data.m_coord[1];
-
-            this->m_coord[2] += data.m_coord[2];
-
-            return *this;
-        }
-
-        const R3 &operator -=(const R3 &data)
-        {
-            this->m_coord[0] -= data.m_coord[0];
-
-            this->m_coord[1] -= data.m_coord[1];
-
-            this->m_coord[2] -= data.m_coord[2];
-
-            return *this;
-        }
-
-        const R3 &operator *=(const double &a)
-        {
-            this->m_coord[0] *= a;
-
-            this->m_coord[1] *= a;
-
-            this->m_coord[2] *= a;
-
-            return *this;
-        }
-
-        const R3 &operator /=(const double &a)
-        {
-            this->m_coord[0] /= a;
-
-            this->m_coord[1] /= a;
-
-            this->m_coord[2] /= a;
+            m_coord[2] = data.m_coord[2];
 
             return *this;
         }
 
         double &operator [](const unsigned i)
         {
-            return this->m_coord[i];
+            return m_coord[i];
         }
 
-        const double &GetCoord(const unsigned i)
+        const double &operator [](const unsigned i) const
         {
-            return this->m_coord[i];
+            return m_coord[i];
+        }
+
+        const R3 &operator +=(const R3 &data)
+        {
+            m_coord[0] += data.m_coord[0];
+
+            m_coord[1] += data.m_coord[1];
+
+            m_coord[2] += data.m_coord[2];
+
+            return *this;
+        }
+
+        const R3 &operator -=(const R3 &data)
+        {
+            m_coord[0] -= data.m_coord[0];
+
+            m_coord[1] -= data.m_coord[1];
+
+            m_coord[2] -= data.m_coord[2];
+
+            return *this;
+        }
+
+        const R3 &operator *=(const double &a)
+        {
+            m_coord[0] *= a;
+
+            m_coord[1] *= a;
+
+            m_coord[2] *= a;
+
+            return *this;
+        }
+
+        const R3 &operator /=(const double &a)
+        {
+            m_coord[0] /= a;
+
+            m_coord[1] /= a;
+
+            m_coord[2] /= a;
+
+            return *this;
+        }
+
+        double &GetCoord(const unsigned i)
+        {
+            return m_coord[i];
+        }
+
+        double *GetCoord()
+        {
+            return m_coord;
+        }
+
+        const double &GetCoord(const unsigned i) const
+        {
+            return m_coord[i];
+        }
+
+        const double *GetCoord() const
+        {
+            return m_coord;
+        }
+
+        double Mag() const
+        {
+            return std::sqrt(m_coord[0] * m_coord[0] +
+                             m_coord[1] * m_coord[1] +
+                             m_coord[2] * m_coord[2]);
+        }
+
+        void Normalize()
+        {
+            double norm = Mag();
+
+            m_coord[0] /= norm;
+
+            m_coord[1] /= norm;
+
+            m_coord[2] /= norm;
+        }
+
+        void Negate()
+        {
+            m_coord[0] = -m_coord[0];
+
+            m_coord[1] = -m_coord[1];
+
+            m_coord[2] = -m_coord[2];
         }
 
         double Dot(const R3 &data)
@@ -143,13 +192,33 @@ namespace NovaType
                       m_coord[1] * data.m_coord[0]);
         }
 
-        friend R3 operator *(const R3 &data, const double &a);
-        friend R3 operator *(const double &a, const R3 &data);
-        friend R3 operator /(const R3 &data, const double &a);
+        friend const R3 operator +(const R3 &a, const R3 &b);
+        friend const R3 operator -(const R3 &a, const R3 &b);
+        friend const R3 operator *(const R3 &data, const double &a);
+        friend const R3 operator *(const double &a, const R3 &data);
+        friend const R3 operator /(const R3 &data, const double &a);
 
     };
 
-    R3 operator *(const R3 &data, const double &a)
+    const R3 operator +(const R3 &a, const R3 &b)
+    {
+        R3 result(a);
+
+        result += b;
+
+        return result;
+    }
+
+    const R3 operator -(const R3 &a, const R3 &b)
+    {
+        R3 result(a);
+
+        result -= b;
+
+        return result;
+    }
+
+    const R3 operator *(const R3 &data, const double &a)
     {
         R3 result(data);
 
@@ -158,7 +227,7 @@ namespace NovaType
         return result;
     }
 
-    R3 operator *(const double &a, const R3 &data)
+    const R3 operator *(const double &a, const R3 &data)
     {
         R3 result(data);
 
@@ -167,7 +236,7 @@ namespace NovaType
         return result;
     }
 
-    R3 operator /(const R3 &data, const double &a)
+    const R3 operator /(const R3 &data, const double &a)
     {
         R3 result(data);
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,123 @@
 #include <iostream>
 
+#include "NovaType/R3.h"
+
+using namespace NovaType;
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Testing vector calculation ..." << std::endl;
+
+    R3 r1(1, 2, 3);
+
+    R3 r2(4, 5, 6);
+
+    R3 r(r1.GetCoord());
+
+    std::cout << r[0] << "    "
+              << r[1] << "    "
+              << r[2] << std::endl;
+
+    r = r1;
+
+    std::cout << r[0] << "    "
+              << r[1] << "    "
+              << r[2] << std::endl;
+
+    r = r1;
+
+    r = r1 * 2.0;
+
+    std::cout << r[0] << "    "
+              << r[1] << "    "
+              << r[2] << std::endl;
+
+    r = r / 2.0;
+
+    std::cout << r[0] << "    "
+              << r[1] << "    "
+              << r[2] << std::endl;
+
+    r = 2.0 * r1;
+
+    std::cout << r[0] << "    "
+              << r[1] << "    "
+              << r[2] << std::endl;
+
+    r = r1 + r2;
+
+    std::cout << r[0] << "    "
+              << r[1] << "    "
+              << r[2] << std::endl;
+
+    r = r1 - r2;
+
+    std::cout << r[0] << "    "
+              << r[1] << "    "
+              << r[2] << std::endl;
+
+    std::cout << r1.Mag() << std::endl;
+
+    std::cout << r1.Dot(r2) << std::endl;
+
+    r = r1.Cross(r2);
+
+    std::cout << r[0] << "    "
+              << r[1] << "    "
+              << r[2] << std::endl;
+
+    r = r1;
+
+    r.Negate();
+
+    std::cout << r[0] << "    "
+              << r[1] << "    "
+              << r[2] << std::endl;
+
+    r = r1;
+
+    r.Normalize();
+
+    std::cout << r[0] << "    "
+              << r[1] << "    "
+              << r[2] << std::endl;
+
+    r = r1;
+
+    double *coord = r.GetCoord();
+
+    coord[0] = 7;
+
+    r.GetCoord(1) = 8;
+
+    std::cout << r[0] << "    "
+              << r[1] << "    "
+              << r[2] << std::endl;
+
+    const R3 rConst(1, 2, 3);
+
+    std::cout << rConst[0] << "    "
+              << rConst[1] << "    "
+              << rConst[2] << std::endl;
+
+    const double *constCoord = rConst.GetCoord();
+
+    for(unsigned i(0); i < 3; ++i)
+    {
+        std::cout << constCoord[i] << "    ";
+    }
+
+    std::cout << std::endl;
+
+
+    for(unsigned i(0); i < 3; ++i)
+    {
+        std::cout << rConst.GetCoord(i) << "    ";
+    }
+
+    std::cout << std::endl;
+
+
 
     return 0;
 }
