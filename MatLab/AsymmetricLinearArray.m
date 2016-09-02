4cc06991e8b431b38c285d267fc9a338e337529b
noteasy2beme <noteasy2beme@gmail.com>
2016-09-02 15:33:49 -0700

    Uploaded MatLab files used to compute patterns of symmetric an asymmetric linear array

---
+++ MatLab/AsymmetricLinearArray.m
@@ -0,0 +1,100 @@
+clear all;
+close all;
+clc;
+
+% Freespace wavelength:
+
+lambda = 1;
+
+% Freespace wave number:
+
+beta = 2 * pi / lambda;
+
+%% Distance between elements:
+%
+%d = 0.7559 * lambda;
+
+%d = 0.45 * lambda;
+
+% Microstrip wavelength:
+
+lambdaMicrostrip = lambda / sqrt(1.75);
+
+% Microstrip wave number:
+
+betaMicrostrip = 2 * pi / lambdaMicrostrip;
+
+% Phase difference of excitation of adjacent elements:
+
+alpha = 2*pi;
+
+d = alpha / betaMicrostrip;
+
+% Angle:
+
+theta = (0 : 0.5 : 180) * pi / 180;
+
+% Number of elements:
+
+N = 9;
+
+% Magnitudes of excitation:
+
+A = ones(N, 1);
+
+% Array factor:
+
+AF = zeros(length(theta), 1);
+
+for m = 1 : length(theta)
+
+  for n = 1 : N
+  
+    phi = beta * d * cos(theta(m));
+    
+    AF(m) = AF(m) + A(n) * exp(1j * (n - 1) * (phi + alpha)) * sin(theta(m));
+  end
+
+end
+
+AF = abs(AF);
+
+maxAF = max(AF);
+
+AF1 = AF / maxAF;
+
+% Number of elements:
+
+N = 13;
+
+% Magnitudes of excitation:
+
+A = ones(N, 1);
+
+% Array factor:
+
+AF = zeros(length(theta), 1);
+
+for m = 1 : length(theta)
+
+  for n = 1 : N
+  
+    phi = beta * d * cos(theta(m));
+    
+    AF(m) = AF(m) + A(n) * exp(1j * (n - 1) * (phi + alpha)) * sin(theta(m));
+  end
+
+end
+
+AF = abs(AF);
+
+maxAF = max(AF);
+
+AF2 = AF / maxAF;
+
+figure(1);
+
+plot(theta / pi * 180, AF1, 'b-', ...
+     theta / pi * 180, AF2, 'r-');
+     
+grid on;
